4ede85b1975e722ddb2f7ff91a5f0e24c733db46
diegri <diegri@gmail.com>
2025-05-16 13:03:33 +0200

    add change to PR

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+    }
+    public static void main( String[] args, String[] args2 )
+    {
+        System.out.println( "Hello World!" );
+
     }
 }
+
--- target/classes/archetype-resources/src/main/java/App.java
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!" );
+        System.out.println( "Hello World!" );
+
+    }
+    public static void main( String[] args, String[] args2 )
+    {
+        System.out.println( "Hello World!" );
+
     }
 }
+
